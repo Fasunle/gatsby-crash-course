bc9227c297218e7cacb5b1ab61467d5d54f337e3
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-05-03 16:14:09 +0100

    Setup Gatsby with Tailwindcss

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => (
   <div>
-    <h1>My Landing Page</h1>
+    <h1 className="text-3xl font-bold text-blue-600">My Landing Page</h1>
     <p>Thuis is my Landing Page</p>
   </div>
 );
