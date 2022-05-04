7d5db7d7fb1049f3443de9339b9c10c51a1c3a61
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-05-04 15:24:32 +0100

    configured gatsby to use env variable and gatsby-source-graphcms and gatsby-plugin-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 module.exports = {
   siteMetadata: {
     siteUrl: "http://localhost:8000",
@@ -7,12 +9,19 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: "gatsby-source-graphcms",
+      options: {
+        endpoint: process.env.GRAPHCMS_ENDPOINT,
+      },
+    },
+    {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "mdx-bio",
         path: `${__dirname}/MDX`,
       },
     },
+    `gatsby-plugin-image`,
     `gatsby-plugin-mdx`,
     "gatsby-plugin-postcss",
   ],
